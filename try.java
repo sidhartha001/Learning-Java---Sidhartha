aaada4b14f1adfff97e4477645eb7d972ccba4de
Sarthak Mohanty <sarthakmohanty200@gmail.com>
2021-07-01 14:09:54 +0530

    first commit

---
+++ try.java
@@ -0,0 +1,13 @@
+class LinkedList {
+    Node head;
+    class Node {
+        int info;
+        Node next;
+        Node(int i) {
+            this.info = i;
+            this.next = null;
+            Node obj = new Node(3);
+            obj.next = s
+        }
+    } s
+}

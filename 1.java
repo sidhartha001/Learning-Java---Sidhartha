ecf812d5fda6c57071ff5f32cf012f3f1edfdfa6
sidhartha2002 <mohantysidhartha2002@gmail.com>
2021-07-06 23:48:13 +0530

    .

--- 1.java
+++ 1.java
@@ -1,1 +1,34 @@
-1
+           : What Parents Should Do About Allowances :
+Parents have a lot of important decisions to make with regard to their kids.
+One decision that’s sometimes overlooked, although still important, is 
+whether or not to give an allowance. Establishing a system for an 
+allowance may not work for every family, so there are some things 
+to consider when deciding if you should give one to your child.
+Receiving an allowance lets your children start to understand how 
+money works and gives them a chance to learn and practice financial 
+strategies for the future.
+
+Children don’t necessarily deserve allowance from their parents, but 
+tying it to chores in some way lets you start teaching them about 
+finances while avoiding a sense of entitlement.An allowance can teach
+kids about finances, responsibility and the consequences of poor 
+financial decisions.If the allowance is tied to chores, kids learn
+the relationship between work and pay. Helping your child understand
+the correlation between work and reward will set them up for their 
+future careers.If you require donating as part of your allowance system,
+this can teach your kids the importance of giving to those who are 
+less fortunate.
+
+Even though an allowance can be a great teaching tool, there are some downsides.
+An allowance may undermine the importance of contributing to the family. 
+Your kids may get the perception that duties always deserve a reward instead
+of simply doing their share for the family.
+An allowance may open the door to kids making poor financial decisions
+when spending their money.
+Every family is different. Determine what may work for you and your family.
+Establish a clear system and guidelines. There is no one right way to give
+your kids an allowance.
+Every family is different. Determine what may work for you and your family.
+Establish a clear system and guidelines. There is no one right way to 
+give your kids an allowance. When it comes to allowances, 
+it's best to know what your child would typically be spending the money on. 
